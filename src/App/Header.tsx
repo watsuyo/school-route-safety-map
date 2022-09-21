973f4fb5acff81631d350cd6bd4b3efdab64951f
watsuyo <watsuyo.dev@gmail.com>
2022-09-22 02:18:48 +0900

    Header 作成

---
+++ src/App/Header.tsx
@@ -0,0 +1,12 @@
+import './Header.scss'
+
+const Content = () => {
+  return (
+    <div className="head">
+      <p>通学路安全マップ</p>
+      {/* <button ><AiOutlineClose size="16px" color="#FFFFFF" /> 閉じる</button> */}
+    </div>
+  )
+}
+
+export default Content
