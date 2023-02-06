72fa089f66f22c1cd5524acdd5bc2927282b5a42
watsuyo <watsuyo.dev@gmail.com>
2023-02-07 04:42:27 +0900

    交通事故情報をひとまとめ

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,12 @@ import Home from './App/Home'
 import List from './App/List'
 import Tabbar from './App/Tabbar'
 import Post from './App/Post'
-import { getSafetyData } from "./api"
+// import { getSafetyData } from "./api"
+import schoolList from './lib/suginami_202.json'
+import schoolZoneList from './lib/suginami_201.json'
+import honhyo2019List from './lib/honhyo_2019.json'
+import honhyo2020List from './lib/honhyo_2020.json'
+import honhyo2021List from './lib/honhyo_2021.json'
 
 const sortShopList = async (shopList: Pwamap.ShopData[]) => {
 
@@ -37,15 +42,80 @@ const App = () => {
         }
 
         (async () => {
-          const res = await getSafetyData()
-          const features = JSON.parse(res)
+          const schoolListFeatures = schoolList.data.map((school) => {
+            return {
+              'スポット名': school["\uFEFF\"\u540D\u524D\""],
+              '緯度': school['緯度'],
+              '経度': school['経度'],
+              'タイムスタンプ': '',
+              'カテゴリ': '',
+              '紹介文': '',
+              'いいね数': 0,
+            }
+          })
+
+          const zoneFeatures = schoolZoneList.data.map((school) => {
+            return {
+              'スポット名': '通学路',
+              '緯度': school['緯度'],
+              '経度': school['経度'],
+              'タイムスタンプ': '',
+              'カテゴリ': '',
+              '紹介文': '',
+              'いいね数': 0,
+            }
+          })
+
+          const honhyoListFeatures = honhyo2019List.data.concat(honhyo2020List.data).concat(honhyo2021List.data).filter((item) => {
+            const timestamp = new Date(item['発生日時　　年'] + '/' + item['発生日時　　月'] + '/' + item['発生日時　　日'] + ' ' + item['発生日時　　時'] + ':' + item['発生日時　　分'])
+            const hours = timestamp.getHours()
+            const minutes = timestamp.getMinutes()
+
+            const isWithinMorningRange = (hours == 7 && minutes >= 30) || (hours == 8 && minutes < 30)
+            const isWithinAfternoonRange = (hours >= 14 && hours < 18)
+
+            if (isWithinMorningRange || isWithinAfternoonRange) {
+              return item['曜日(発生年月日)'] !== '土' && item['曜日(発生年月日)'] !== '日' && item['祝日(発生年月日)'] !== '1'
+            } else {
+              return false
+            }
+          }).map((item) => {
+            const lat = () => {
+              const lat = String(Number(item['地点　緯度（北緯）']) / 1000)
+              const latDegrees = lat.slice(0, 2)
+              const latMinutes = lat.slice(2, 4)
+              const latSeconds = lat.slice(4)
+              return (Number(latDegrees) + (Number(latMinutes) / 60) + (Number(latSeconds) / 60 / 60)).toFixed(7)
+            }
+
+            const lng = () => {
+              const lng = String(Number(item['地点　経度（東経）']) / 1000)
+              const lngDegrees = lng.slice(0, 3)
+              const lngMinutes = lng.slice(3, 5)
+              const lngSeconds = lng.slice(5)
+              return (Number(lngDegrees) + (Number(lngMinutes) / 60) + (Number(lngSeconds) / 60 / 60)).toFixed(7)
+            }
+
+            const timestamp = () => {
+              return item['発生日時　　年'] + '/' + item['発生日時　　月'] + '/' + item['発生日時　　日'] + ' ' + item['発生日時　　時'] + ':' + item['発生日時　　分']
+            }
+            return {
+              'スポット名': '交通事故',
+              '緯度': lat(),
+              '経度': lng(),
+              'タイムスタンプ': timestamp(),
+              'カテゴリ': '交通事故',
+              '紹介文': '',
+              'いいね数': 0,
+            }
+          })
+
 
           setIsLoading(false)
-
-          if (features.length) {
+          if (honhyoListFeatures.length) {
             const nextShopList: Pwamap.ShopData[] = []
-            for (let i = 0;i < features.length;i++) {
-              const feature = features[i] as Pwamap.ShopData
+            for (let i = 0;i < honhyoListFeatures.length;i++) {
+              const feature = honhyoListFeatures[i]
               if (!feature['緯度'] || !feature['経度'] || !feature['スポット名']) {
                 continue
               }
