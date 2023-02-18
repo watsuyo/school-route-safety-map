a9dd25774cef086091147f3228616ed12cc11247
watsuyo <watsuyo.dev@gmail.com>
2023-02-18 20:34:29 +0900

    serviceWorkerRegistration を使わない

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from "react-router-dom";
 import Container from './Container';
 import './index.scss'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,4 +18,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
